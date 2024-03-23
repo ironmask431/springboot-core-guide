2551d6cce0044297e683e5bdc43c36c920d15526
ironmask431 <ironmask431@gmail.com>
2024-03-23 21:03:04 +0900

    doc: 불필요코드 삭제

--- batch-api/src/main/java/kevin/springboot/core/guide/BatchApiApplication.java
+++ batch-api/src/main/java/kevin/springboot/core/guide/BatchApiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * 멀티모듈 Application 파일 생성 시 유의 사항.
  * 참고하려는 모듈 (root모듈 또는 별도로 만든 core모듈) 의 application 파일과
- * 패키지명이 동일해야 한다. 그래야 멀티모듈이 참고 모듈의 bean들을 읽을 수 있고, 설정들을 공유 가능해진다.
+ * 패키지명이 동일해야 한다. 그래야 참고모듈의 security 설정등이 멀티모듈에도 똑같이 적용된다.
  */
 
 @SpringBootApplication
