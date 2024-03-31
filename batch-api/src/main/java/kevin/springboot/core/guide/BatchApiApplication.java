2135e2ce47486f0f15a426d67a32944a7a143164
ironmask431 <ironmask431@gmail.com>
2024-03-31 22:47:01 +0900

    doc: 주석 수정

--- batch-api/src/main/java/kevin/springboot/core/guide/BatchApiApplication.java
+++ batch-api/src/main/java/kevin/springboot/core/guide/BatchApiApplication.java
@@ -5,8 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 멀티모듈 Application 파일 생성 시 유의 사항.
- * 참고하려는 모듈 (root모듈 또는 별도로 만든 core모듈) 의 application 파일과
- * 패키지명이 동일해야 한다. 그래야 참고모듈의 security 설정등 각종 설정이 멀티모듈에도 똑같이 적용된다.
+ * 의존 하려는 모듈 (root모듈 또는 별도로 만든 core모듈) 의 application 파일과
+ * 패키지명이 동일해야 한다. 그래야 참고모듈의 spring security, controllerAdvice, Swagger 등 각종 설정이 멀티모듈 에도 똑같이 적용된다.
  * 모듈들의 기본 상위 패키지명은 모두 동일하게 가는게 설정 공유하기 좋을 듯 하다.
  */
 
