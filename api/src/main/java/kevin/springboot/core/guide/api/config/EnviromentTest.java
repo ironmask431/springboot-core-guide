5cc89292ec388a9281fbf7a182700b178c22e01a
ironmask431 <ironmask431@gmail.com>
2024-04-14 20:37:00 +0900

    feat: profile을 이용하여 환경별로 다른 yml 설정 적용

---
+++ api/src/main/java/kevin/springboot/core/guide/api/config/EnviromentTest.java
@@ -0,0 +1,33 @@
+package kevin.springboot.core.guide.api.config;
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.core.env.Environment;
+import org.springframework.core.env.Profiles;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class EnviromentTest {
+    private final Environment env;
+
+
+    //jar 구동 시 VM option 설정 :  -Dspring.profiles.active=prd
+    @PostConstruct // 의존성 주입이 완료된 후 메소드가 실행되도록 함.
+    public void init() {
+        log.info("EnviromentTest url = {}", env.getProperty("url"));
+        log.info("EnviromentTest env.getActiveProfiles() = {}", env.getActiveProfiles());
+        if (env.acceptsProfiles(Profiles.of("dev"))) {
+            log.info("this env on dev.");
+        }
+        if (env.acceptsProfiles(Profiles.of("prd"))) {
+            log.info("this env on prd.");
+        }
+
+    }
+
+}
