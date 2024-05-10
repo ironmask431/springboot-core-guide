bab3d07a8979c713111dee05dd94f849918e3653
ironmask431 <ironmask431@gmail.com>
2024-05-10 17:09:49 +0900

    doc: 주석추가, 파일명 변경

--- api/src/main/java/kevin/springboot/core/guide/api/config/EnviromentTest.java
+++ api/src/main/java/kevin/springboot/core/guide/api/config/EnviromentTest.java
@@ -3,6 +3,7 @@ package kevin.springboot.core.guide.api.config;
 import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
 import org.springframework.stereotype.Component;
@@ -14,20 +15,20 @@ import java.util.Arrays;
 @Slf4j
 public class EnviromentTest {
     private final Environment env;
-
-
     //jar 구동 시 VM option 설정 :  -Dspring.profiles.active=prd
     @PostConstruct // 의존성 주입이 완료된 후 메소드가 실행되도록 함.
     public void init() {
+        //yml파일의 url 값 조회
         log.info("EnviromentTest url = {}", env.getProperty("url"));
+
+        //현재 활성화된 환경변수 확인
         log.info("EnviromentTest env.getActiveProfiles() = {}", env.getActiveProfiles());
+
         if (env.acceptsProfiles(Profiles.of("dev"))) {
             log.info("this env on dev.");
         }
         if (env.acceptsProfiles(Profiles.of("prd"))) {
             log.info("this env on prd.");
         }
-
     }
-
 }
